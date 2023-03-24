5bfa7ff17419e93eba536b5977a3262dc551d20e
Bryden Frizzell <bryden.frizzell@gmail.com>
2023-03-25 10:56:09 +1300

    Removed Steam 1.0.60 support, added Steam 1.0.62 + 1.0.64 support

--- RE_Kenshi_en.ts
+++ RE_Kenshi_en.ts
@@ -35,73 +35,73 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="51"/>
+        <location filename="installwindow.cpp" line="70"/>
         <source>Double-checking hash...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="64"/>
+        <location filename="installwindow.cpp" line="83"/>
         <source>Hash matches. Uninstalling old RE_Kenshi modifications...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="69"/>
+        <location filename="installwindow.cpp" line="88"/>
         <source>Hash matches. Making kenshi plugin config backup...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="76"/>
+        <location filename="installwindow.cpp" line="95"/>
         <source>Plugin config already backed up, skipping...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="100"/>
+        <location filename="installwindow.cpp" line="119"/>
         <source>Copying mod files...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="138"/>
+        <location filename="installwindow.cpp" line="156"/>
         <source>Updating mod config...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="147"/>
+        <location filename="installwindow.cpp" line="165"/>
         <source>Error copying translation files. </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="176"/>
+        <location filename="installwindow.cpp" line="194"/>
         <source>Compressing heightmap, this may take a minute or two...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="190"/>
-        <location filename="installwindow.cpp" line="206"/>
+        <location filename="installwindow.cpp" line="208"/>
+        <location filename="installwindow.cpp" line="224"/>
         <source>Adding RE_Kenshi to plugin config file...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="223"/>
+        <location filename="installwindow.cpp" line="242"/>
         <source>UNCAUGHT ERROR?!? Sorry, I probably broke your kenshi install. Rename &quot;kenshi_x64_vanilla.exe&quot; to &quot;kenshi_x64.exe&quot; and &quot;Plugins_x64_vanilla.cfg&quot; to &quot;Plugins_x64.cfg&quot; to fix whatever I&apos;ve done... :(</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="227"/>
+        <location filename="installwindow.cpp" line="246"/>
         <source>RE_Kenshi has installed successfully!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="236"/>
+        <location filename="installwindow.cpp" line="255"/>
         <source>Error: Shell command returned: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="236"/>
+        <location filename="installwindow.cpp" line="255"/>
         <source> install aborted.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="installwindow.cpp" line="245"/>
+        <location filename="installwindow.cpp" line="264"/>
         <source>Error: </source>
         <translation type="unfinished"></translation>
     </message>
@@ -114,26 +114,26 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="27"/>
-        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="mainwindow.ui" line="124"/>
+        <location filename="mainwindow.cpp" line="176"/>
         <source>Find Kenshi install dir</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="53"/>
-        <location filename="mainwindow.cpp" line="173"/>
+        <location filename="mainwindow.cpp" line="177"/>
         <source>Next</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="66"/>
-        <location filename="mainwindow.cpp" line="171"/>
+        <location filename="mainwindow.cpp" line="175"/>
         <source>Please select kenshi executable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="88"/>
-        <location filename="mainwindow.cpp" line="174"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>Uninstall</source>
         <translation type="unfinished"></translation>
     </message>
@@ -143,62 +143,72 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="39"/>
+        <location filename="mainwindow.cpp" line="43"/>
         <source>Select Kenshi executable:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="53"/>
+        <location filename="mainwindow.cpp" line="57"/>
         <source>Processing...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="75"/>
+        <location filename="mainwindow.cpp" line="79"/>
         <source>Checking file hash...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="87"/>
+        <location filename="mainwindow.cpp" line="91"/>
         <source>Please select kenshi executable.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="93"/>
+        <location filename="mainwindow.cpp" line="97"/>
         <source>Error hashing file. </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="93"/>
+        <location filename="mainwindow.cpp" line="97"/>
         <source> Please select kenshi executable.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="124"/>
+        <location filename="mainwindow.cpp" line="128"/>
         <source>Game hash matches. Continue...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="135"/>
+        <location filename="mainwindow.cpp" line="139"/>
         <source>Hash </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="135"/>
-        <source> does not match. This mod is only compatible with Kenshi 1.0.55, 1.0.59 (Steam) and 1.0.59 (GOG).</source>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source> does not match. This mod is only compatible with Kenshi </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="151"/>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source> (Steam) and </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source> (GOG).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="155"/>
         <source>Are you sure you wish to uninstall RE_Kenshi?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="152"/>
+        <location filename="mainwindow.cpp" line="156"/>
         <source>Confirm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="153"/>
+        <location filename="mainwindow.cpp" line="157"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -242,81 +252,164 @@ Heightmap compression significantly improves load times on HDDs and may improve 
         <source>Clear skipped versions</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <location filename="optionswindow.cpp" line="23"/>
+        <source>Installing to HDD, heightmap compression enabled by default.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="optionswindow.cpp" line="28"/>
+        <source>Not installing to HDD, heightmap compression disabled by default.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="optionswindow.cpp" line="29"/>
+        <source>Heightmap compression may or may not improve performance on your system.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="bugs.cpp" line="23"/>
+        <location filename="bugs.cpp" line="86"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="24"/>
+        <source>RE_Kenshi&apos;s installer has encountered an error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="25"/>
+        <source>
+Would you like to send an error report to the RE_Kenshi team?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="26"/>
+        <source>
+Your report will be sent to RE_Kenshi&apos;s developer (BFrizzleFoShizzle) with the following information:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="27"/>
+        <source>
+Installer version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="28"/>
+        <source>
+Window: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="29"/>
+        <source>
+Installation step: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="30"/>
+        <source>
+Error message: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="75"/>
+        <source>Bug report sent successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="76"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="bugs.cpp" line="85"/>
+        <source>Bug report failed to send</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>UninstallWindow</name>
     <message>
-        <location filename="uninstallwindow.cpp" line="69"/>
+        <location filename="uninstallwindow.cpp" line="86"/>
         <source>EXE hash matches old RE_Kenshi mod. Uninstalling old RE_Kenshi EXE modifications...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="79"/>
+        <location filename="uninstallwindow.cpp" line="96"/>
         <source>EXE hash matches vanilla kenshi. Reverting kenshi plugin config to backup...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="93"/>
+        <location filename="uninstallwindow.cpp" line="110"/>
         <source>Critical error: no config file backup!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="100"/>
+        <location filename="uninstallwindow.cpp" line="118"/>
         <source>Hash doesn&apos;t match! This shouldn&apos;t be possible! No files changed, aborted. Mod not installed. It is now safe to close this window.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="111"/>
+        <location filename="uninstallwindow.cpp" line="130"/>
         <source>Backup hash matches, restoring vanilla kenshi EXE.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="123"/>
+        <location filename="uninstallwindow.cpp" line="142"/>
         <source>Backup EXE hash doesn&apos;t match! No files changed, aborted. Automatic uninstallation failed. It is now safe to close this window.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="131"/>
+        <location filename="uninstallwindow.cpp" line="150"/>
         <source>Old files restored. Removing RE_Kenshi DLL...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="143"/>
+        <location filename="uninstallwindow.cpp" line="162"/>
         <source>Old files restored. Removing CompressTools DLL...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="155"/>
+        <location filename="uninstallwindow.cpp" line="174"/>
         <source>Old files restored. Removing RE_Kenshi data...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="177"/>
+        <location filename="uninstallwindow.cpp" line="196"/>
         <source>Old files restored. Removing compressed heightmap...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="197"/>
+        <location filename="uninstallwindow.cpp" line="217"/>
         <source>UNCAUGHT ERROR?!? Sorry, I probably broke your kenshi install. Rename &quot;kenshi_x64_vanilla.exe&quot; to &quot;kenshi_x64.exe&quot; and &quot;Plugins_x64_vanilla.cfg&quot; to &quot;Plugins_x64.cfg&quot; to fix whatever I&apos;ve done... :(</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="205"/>
+        <location filename="uninstallwindow.cpp" line="225"/>
         <source>RE_Kenshi has uninstalled successfully!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="221"/>
+        <location filename="uninstallwindow.cpp" line="241"/>
         <source>Error: Shell command returned: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="221"/>
+        <location filename="uninstallwindow.cpp" line="241"/>
         <source> install aborted.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="uninstallwindow.cpp" line="230"/>
+        <location filename="uninstallwindow.cpp" line="250"/>
         <source>Error: </source>
         <translation type="unfinished"></translation>
     </message>
--- hashthread.cpp
+++ hashthread.cpp
@@ -10,8 +10,8 @@
 const std::string moddedKenshiSteamHash = "a5f78908f3f26591a6a3717bfbfafbca";
 const std::string vanillaKenshiHashes[] = {
     "83ea507cf9667bfe8de2d8a64e9ea57a", // Steam 1.0.55
-    "3327eaf22ec3c9653e22b6d7bf351736", // Steam 1.0.59
-    "488c2f2795212943c12283d3c67cf52f", // Steam 1.0.60
+    "85ff75251ed9bca7629ee427978088aa", // Steam 1.0.62
+    "631763f867f8a674fc54a78cd5767235", // Steam 1.0.64
     "c74140b9ac13995500cd1413b8cc0ba2"  // GOG 1.0.59
 };
 
