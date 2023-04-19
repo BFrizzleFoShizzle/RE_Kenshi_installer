2f8ba78377dd8ff780174a6c92c5ca5d780713a9
Bryden Frizzell <bryden.frizzell@gmail.com>
2023-04-19 16:40:51 +1200

    Removed Steam 1.0.62 support

--- hashthread.cpp
+++ hashthread.cpp
@@ -9,8 +9,7 @@
 
 const std::string moddedKenshiSteamHash = "a5f78908f3f26591a6a3717bfbfafbca";
 const std::string vanillaKenshiHashes[] = {
-    "83ea507cf9667bfe8de2d8a64e9ea57a", // Steam 1.0.55
-    "85ff75251ed9bca7629ee427978088aa", // Steam 1.0.62
+	"83ea507cf9667bfe8de2d8a64e9ea57a", // Steam 1.0.55
     "631763f867f8a674fc54a78cd5767235", // Steam 1.0.64
     "c74140b9ac13995500cd1413b8cc0ba2"  // GOG 1.0.59
 };
